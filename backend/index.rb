30a6df581fd6dcd3ed45a5828a5250988985256b
Karolis1 <karolis.greblikas@gmail.com>
2014-11-01 21:02:04 +0000

    backend new api for questions

--- backend/index.rb
+++ backend/index.rb
@@ -40,12 +40,43 @@ module ReadingExpert
       end
     end
 
+    def self.get_questions_by_id(id)
+      begin
+        db = Mysql.real_connect('localhost', 'rails', 'kXI6cjTJCO', 'rails')
+ 
+        id = id.to_s
+        query = 'SELECT * FROM Questions WHERE story_id = ' + id
+        puts query
+        results = db.query(query)
+
+        response = []
+        results.each_hash do |row|
+          response.push(row)
+        end
+        response
+      ensure
+        db.close
+      end
+    end
+
+
     resource :stories do
       get :get_all_stories do
     	API.get_all_stories
       end
+      
       get :get_all_questions do
         API.get_all_questions
+      end
+      
+      params do
+        requires :id, type: Integer
+      end
+      route_param :question_by_id do
+        get do
+          puts params['id'] 
+          API.get_questions_by_id(params['id'])
+        end
       end
     end
 
