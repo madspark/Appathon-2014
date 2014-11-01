6efa100e20dfc91ff777ebec69a4c22ea925bf94
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2014-11-01 14:24:18 +0000

    Button from main activity to hear and type activity

--- ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/MainActivity.java
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/MainActivity.java
@@ -1,9 +1,12 @@
 package lt.vadovauk.readingexpert.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button hearAndTypeButton = (Button) findViewById(R.id.hear_and_type);
+        hearAndTypeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ReadAndTypeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
