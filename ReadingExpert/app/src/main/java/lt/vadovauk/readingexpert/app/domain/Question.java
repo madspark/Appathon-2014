054b82f038efeddc6e2757eab83a03e27da618c4
Antanas Kascenas <antanas@kascenas.com>
2014-11-01 17:54:05 +0000

    Fixes to DbContract, DbHelper. Helper mothods in domain module

--- ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/domain/Question.java
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/domain/Question.java
@@ -1,11 +1,22 @@
 package lt.vadovauk.readingexpert.app.domain;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import lt.vadovauk.readingexpert.app.data.DbContract;
+import lt.vadovauk.readingexpert.app.data.DbHelper;
 
 public class Question {
     private int id;
     private int storyId;
     private String question;
+    private String answer;
+    ArrayList<String> otherAnswers;
 
     public ArrayList<String> getOtherAnswers() {
         return otherAnswers;
@@ -47,14 +58,58 @@ public class Question {
         this.answer = answer;
     }
 
-    private String answer;
-    ArrayList<String> otherAnswers;
-
     public Question(int id, int storyId, String question, String answer, ArrayList<String> otherAnswers) {
         this.id = id;
         this.storyId = storyId;
         this.question = question;
         this.answer = answer;
         this.otherAnswers = otherAnswers;
+    }
+
+    public void insertIntoDb(Context context){
+        ContentValues cv = new ContentValues();
+        cv.put(DbContract.Question.COLUMN_STORYID, storyId);
+        cv.put(DbContract.Question.COLUMN_CONTENT, question);
+        cv.put(DbContract.Question.COLUMN_CORRECT, answer);
+        String other = "";
+        for(int i =0 ;i < otherAnswers.size(); i++){
+            if(i != 0)other += ",";
+            other += otherAnswers.get(i);
+        }
+        cv.put(DbContract.Question.COLUMN_OTHER, other);
+
+        DbHelper helper = new DbHelper(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.insert(DbContract.Question.TABLE_NAME, null, cv);
+        db.close();
+    }
+
+    public static Question[] getQuestions(int storyId, Context context){
+
+        DbHelper helper = new DbHelper(context);
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String question;
+        String correct;
+        ArrayList<String> others;
+
+        Cursor c = db.query(DbContract.Question.TABLE_NAME, null,
+                DbContract.Question.COLUMN_STORYID + " = ?",
+                new String[] {Integer.toString(storyId)}, null, null, null);
+
+        int cursorSize = c.getPosition();
+        Question[] qs = new Question[cursorSize];
+        c.moveToFirst();
+        for(int i = 0; i < cursorSize; i++){
+            question = c.getString(c.getColumnIndex(DbContract.Question.COLUMN_CONTENT));
+            correct = c.getString(c.getColumnIndex(DbContract.Question.COLUMN_CORRECT));
+            String ot = c.getString(c.getColumnIndex(DbContract.Question.COLUMN_OTHER));
+            others = new ArrayList<String>(Arrays.asList(ot.split(",")));
+            int id = c.getInt(c.getColumnIndex(DbContract.Question.COLUMN_ID));
+
+            qs[i] = new Question(id, storyId, question, correct, others);
+        }
+
+        return qs;
     }
 }
