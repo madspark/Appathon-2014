8b2babbec2cf49a333358da6b0483e55e16cb053
Antanas Kascenas <antanas@kascenas.com>
2014-11-01 15:49:13 +0000

    data module initial commit

---
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/data/DbContract.java
@@ -0,0 +1,40 @@
+package lt.vadovauk.readingexpert.app.data;
+
+/**
+ * Created by antanas on 14.11.1.
+ */
+public class DbContract {
+
+    class Story {
+
+        public static final String TABLE_NAME = "Stories";
+
+        public static final String COLUMN_TITLE = "title";
+
+        public static final String COLUMN_ID = "id";
+
+        public static final String COLUMN_DIFF = "difficulty";
+
+        public static final String COLUMN_IMG = "imagesource";
+
+        public static final String COLUMN_DONE = "done";
+
+        public static final String COLUMN_CONTENT = "content";
+
+    }
+
+    class Question {
+
+        public static final String TABLE_NAME = "Questions";
+
+        public static final String COLUMN_ID = "Qid";
+
+        public static final String COLUMN_STORYID = "StoryID";
+
+        public static final String COLUMN_CONTENT = "Qcontent";
+
+        public static final String COLUMN_CORRECT = "correct answer";
+
+        public static final String COLUMN_OTHER = "Other answers";
+    }
+}
---
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/data/DbHelper.java
@@ -0,0 +1,56 @@
+package lt.vadovauk.readingexpert.app.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by antanas on 14.11.1.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_NAME = "entries.db";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE_ENTRIES_TABLE = "CREATE TABLE " + DbContract.Story.TABLE_NAME +
+                " (" +
+                DbContract.Story.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                DbContract.Story.COLUMN_DIFF + " INTEGER NOT NULL, " +
+                DbContract.Story.COLUMN_IMG + " TEXT NOT NULL, " +
+                DbContract.Story.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                DbContract.Story.COLUMN_TITLE + " TEXT NOT NULL, " +
+                DbContract.Story.COLUMN_DONE + " BOOL, " +
+                "UNIQUE (" + DbContract.Story.COLUMN_ID + ") ON CONFLICT REPLACE );"
+                +
+                "CREATE TABLE " + DbContract.Question.TABLE_NAME + " (" +
+                DbContract.Question.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                DbContract.Question.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                DbContract.Question.COLUMN_CORRECT + " TEXT NOT NULL, " +
+                DbContract.Question.COLUMN_OTHER + " TEXT NOT NULL, " +
+                DbContract.Question.COLUMN_STORYID + " INTEGER NOT NULL, " +
+                "UNIQUE (" + DbContract.Question.COLUMN_ID + ") ON CONFLICT REPLACE );";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        //Fires when DATABASE_VERSION changes
+        //Delete the entries table
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DbContract.Story.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DbContract.Question.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+    }
+}
+
+
