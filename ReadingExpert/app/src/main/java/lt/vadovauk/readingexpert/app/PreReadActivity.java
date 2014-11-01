c0121d7a27571a03dc865a76d1b4a81e15c546ac
Mantas <mantasmiksys3@gmail.com>
2014-11-01 15:22:25 +0000

    added PreRead, Read, Question activities

--- ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/PreReadActivity.java
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/PreReadActivity.java
@@ -1,32 +1,32 @@
 package lt.vadovauk.readingexpert.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class PreReadActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pre_read);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.pre_read, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
