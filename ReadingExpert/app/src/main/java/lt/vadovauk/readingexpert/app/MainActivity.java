90579d714fea250c752654a1b69005dd6be639f7
Antanas Kascenas <antanas@kascenas.com>
2014-11-01 16:37:16 +0000

    MainActivity nothing changed

--- ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/MainActivity.java
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/MainActivity.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends Activity {
 
