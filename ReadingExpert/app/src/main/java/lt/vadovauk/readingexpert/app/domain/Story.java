b7e052c9523c9d6ee77f2e8072d85cb53d579809
Antanas Kascenas <antanas@kascenas.com>
2014-11-01 20:51:11 +0000

    updated for use with apiID

--- ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/domain/Story.java
+++ ReadingExpert/app/src/main/java/lt/vadovauk/readingexpert/app/domain/Story.java
@@ -9,6 +9,7 @@ import lt.vadovauk.readingexpert.app.data.DbContract;
 import lt.vadovauk.readingexpert.app.data.DbHelper;
 
 public class Story {
+    private int apiid;
     private String title;
     private String description;
     private int difficulty; //wpm
@@ -16,7 +17,7 @@ public class Story {
     private String imageSource;
     private boolean done;
 
-    public Story(String title, String description, int difficulty,
+    public Story(int apiid, String title, String description, int difficulty,
                  String content, String imageSource) {
         this.title = title;
         this.description = description;
@@ -68,6 +69,7 @@ public class Story {
 
     public void insertIntoDb(Context context) {
         ContentValues cv = new ContentValues();
+        cv.put(DbContract.Story.COLUMN_APIID, apiid);
         cv.put(DbContract.Story.COLUMN_TITLE, title);
         cv.put(DbContract.Story.COLUMN_DIFF, difficulty);
         cv.put(DbContract.Story.COLUMN_DONE, false); // Assume story is not completed at the point of insertion
@@ -81,13 +83,13 @@ public class Story {
         db.close();
     }
 
-    public static Story getStory(int id, Context context) {
+    public static Story getStory(int apiid, Context context) {
 
         DbHelper helper = new DbHelper(context);
         SQLiteDatabase db = helper.getReadableDatabase();
         Cursor c = db.query(DbContract.Story.TABLE_NAME, null,
-                DbContract.Story.COLUMN_ID + "= ?",
-                new String[]{Integer.toString(id)}, null, null, null);
+                DbContract.Story.COLUMN_APIID + "= ?",
+                new String[]{Integer.toString(apiid)}, null, null, null);
 
         c.moveToFirst();
 
@@ -98,6 +100,7 @@ public class Story {
         String imageSource;
         boolean done;
 
+        apiid = c.getInt(c.getColumnIndex(DbContract.Story.COLUMN_APIID));
         title = c.getString(c.getColumnIndex(DbContract.Story.COLUMN_TITLE));
         description = c.getString(c.getColumnIndex(DbContract.Story.COLUMN_DESC));
         difficulty = c.getInt(c.getColumnIndex(DbContract.Story.COLUMN_DIFF));
@@ -105,7 +108,7 @@ public class Story {
         imageSource = c.getString(c.getColumnIndex(DbContract.Story.COLUMN_IMG));
         done = 1 == c.getInt(c.getColumnIndex(DbContract.Story.COLUMN_DONE));
 
-        return new Story(title, description, difficulty, content, imageSource);
+        return new Story(apiid, title, description, difficulty, content, imageSource);
 
     }
 
